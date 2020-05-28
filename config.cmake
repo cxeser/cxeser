d5e634097c66a5b41c7464875006f3f8040317f5
small-pony <2262713487@qq.com>
2020-05-28 20:13:09 +0800

    Change name

--- config.cmake
+++ config.cmake
@@ -5,5 +5,5 @@ set(configFileName "${CMAKE_CURRENT_SOURCE_DIR}/src/config.h")
 
 
 file(WRITE ${configFileName} "\r\n#pragma once\r\n" )
-file(APPEND ${configFileName} "static const char* langVersion=\"${lang_VERSION_MAJOR}.${lang_VERSION_MINOR}.${lang_VERSION_PATCH}\";")
-file(APPEND ${configFileName} "\r\nstatic const char* langBuildTime=\"${buildTime}\";")
+file(APPEND ${configFileName} "static const char* cxjserVersion=\"${cxjser_VERSION_MAJOR}.${cxjser_VERSION_MINOR}.${cxjser_VERSION_PATCH}\";")
+file(APPEND ${configFileName} "\r\nstatic const char* cxjserBuildTime=\"${buildTime}\";")
