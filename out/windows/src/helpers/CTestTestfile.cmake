06152bc5bee63e5bdc3d00d1207a5e8ae6b0304c
small-pony <2262713487@qq.com>
2020-05-31 21:11:42 +0800

    add ignore

--- out/windows/src/helpers/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/code/c,cpp/cxjser/src/helpers
-# Build directory: E:/code/c,cpp/cxjser/out/windows/src/helpers
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- out/windows/src/lexer/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/code/c,cpp/cxjser/src/lexer
-# Build directory: E:/code/c,cpp/cxjser/out/windows/src/lexer
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
